5dd9d8e40d48807fc25329dd609864be76c6f902
jibbalobesjones <jibbalobesjones@gmail.com>
2017-12-19 19:56:12 -0500

    first commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+node.default['haproxy']['members'] =   [{
+    "hostname" => "ec2-34-230-38-252.compute-1.amazonaws.com",
+    "ipaddress" => "34.230.38.252",
+    "port" => 80,
+    "ssl_port" => 80
+    },{
+        "hostname" => "ec2-34-235-148-128.compute-1.amazonaws.com",
+        "ipaddress" => "34.235.148.128",
+        "port" => 80,
+        "ssl_port" => 80
+        }
+]
+
+include_recipe 'haproxy::manual'
